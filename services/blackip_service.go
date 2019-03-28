d5ba81e69748f0bbd24abe0b0f705baf2c65dfcb
abnereel <408666378@qq.com>
2019-03-28 17:14:23 +0800

    update

--- services/blackip_service.go
+++ services/blackip_service.go
@@ -1,9 +1,13 @@
 package services
 
 import (
+	"fmt"
+	"github.com/abnereel/lottery/comm"
 	"github.com/abnereel/lottery/dao"
 	"github.com/abnereel/lottery/datasource"
 	"github.com/abnereel/lottery/models"
+	"github.com/gomodule/redigo/redis"
+	"log"
 )
 
 type BlackipService interface {
@@ -42,14 +46,84 @@ func (s *blackipService) Get(id int) *models.LtBlackip {
 	return s.dao.Get(id)
 }
 
-func (s *blackipService) Update(user *models.LtBlackip, columns []string) error {
-	return s.dao.Update(user, columns)
+func (s *blackipService) Update(data *models.LtBlackip, columns []string) error {
+	// 先更新缓存的数据
+	s.updateByCache(data, columns)
+	// 再更新数据的数据
+	return s.dao.Update(data, columns)
 }
 
-func (s *blackipService) Create(user *models.LtBlackip) error {
-	return s.dao.Create(user)
+func (s *blackipService) Create(data *models.LtBlackip) error {
+	return s.dao.Create(data)
 }
 
+// 根据IP读取IP的黑名单数据
 func (s *blackipService) GetByIp(ip string) *models.LtBlackip {
-	return s.dao.GetByIp(ip)
+	// 先从缓存中读取数据
+	data := s.getByCache(ip)
+	if data == nil || data.Ip == "" {
+		// 再从数据库中读取数据
+		data = s.dao.GetByIp(ip)
+		if data == nil || data.Ip == "" {
+			data = &models.LtBlackip{Ip: ip}
+		}
+		s.setByCache(data)
+	}
+	return data
+}
+
+func (s *blackipService) getByCache(ip string) *models.LtBlackip {
+	// 集群模式，redis缓存
+	key := fmt.Sprintf("info_blackip_%s", ip)
+	rds := datasource.InstanceCache()
+	dataMap, err := redis.StringMap(rds.Do("HGETALL", key))
+	if err != nil {
+		log.Println("blackip_service.getByCache HGETALL key=", key, ", error=", err)
+		return nil
+	}
+	dataIp := comm.GetStringFromStringMap(dataMap, "Ip", "")
+	if dataIp == "" {
+		return nil
+	}
+	data := &models.LtBlackip{
+		Id:         int(comm.GetInt64FromStringMap(dataMap, "Id", 0)),
+		Ip:         dataIp,
+		Blacktime:  int(comm.GetInt64FromStringMap(dataMap, "Blacktime", 0)),
+		SysCreated: int(comm.GetInt64FromStringMap(dataMap, "SysCreated", 0)),
+		SysUpdated: int(comm.GetInt64FromStringMap(dataMap, "SysUpdated", 0)),
+	}
+	return data
+}
+
+func (s *blackipService) setByCache(data *models.LtBlackip) {
+	if data == nil || data.Ip == "" {
+		return
+	}
+	// 集群模式，redis缓存
+	key := fmt.Sprintf("info_blackip_%s", data.Ip)
+	rds := datasource.InstanceCache()
+	// 数据更新到redis缓存
+	params := []interface{}{key}
+	params = append(params, "Ip", data.Ip)
+	if data.Id > 0 {
+		params = append(params, "Blacktime", data.Blacktime)
+		params = append(params, "SysCreated", data.SysCreated)
+		params = append(params, "SysUpdated", data.SysUpdated)
+	}
+	_, err := rds.Do("HMSET", params...)
+	if err != nil {
+		log.Println("blackip_service.setByCache HMSET params=", params, ", error=", err)
+	}
+}
+
+// 数据更新了，直接清空缓存数据
+func (s *blackipService) updateByCache(data *models.LtBlackip, columns []string) {
+	if data == nil || data.Ip == "" {
+		return
+	}
+	// 集群模式，redis缓存
+	key := fmt.Sprintf("info_blackip_%s", data.Ip)
+	rds := datasource.InstanceCache()
+	// 删除redis中的缓存
+	_, _ = rds.Do("DEL", key)
 }
--- services/user_service.go
+++ services/user_service.go
@@ -1,9 +1,13 @@
 package services
 
 import (
+	"fmt"
+	"github.com/abnereel/lottery/comm"
 	"github.com/abnereel/lottery/dao"
 	"github.com/abnereel/lottery/datasource"
 	"github.com/abnereel/lottery/models"
+	"github.com/gomodule/redigo/redis"
+	"log"
 )
 
 type UserService interface {
@@ -33,13 +37,88 @@ func (s *userService) CountAll() int {
 }
 
 func (s *userService) Get(id int) *models.LtUser {
-	return s.dao.Get(id)
+	data := s.getByCache(id)
+	if data == nil || data.Id <= 0 {
+		data = s.dao.Get(id)
+		if data == nil || data.Id <= 0 {
+			data = &models.LtUser{Id: id}
+		}
+		s.setByCache(data)
+	}
+	return data
 }
 
-func (s *userService) Update(user *models.LtUser, columns []string) error {
-	return s.dao.Update(user, columns)
+func (s *userService) Update(data *models.LtUser, columns []string) error {
+	s.updateByCache(data, columns)
+	return s.dao.Update(data, columns)
 }
 
-func (s *userService) Create(user *models.LtUser) error {
-	return s.dao.Create(user)
+func (s *userService) Create(data *models.LtUser) error {
+	return s.dao.Create(data)
+}
+
+// 从缓存中得到信息
+func (s *userService) getByCache(id int) *models.LtUser {
+	key := fmt.Sprintf("info_user_%d", id)
+	rds := datasource.InstanceCache()
+	dataMap, err := redis.StringMap(rds.Do("HGETALL", key))
+	if err != nil {
+		log.Println("user_service.getByCache HGETALL key=", key, ", error=", err)
+		return nil
+	}
+	dataId := comm.GetInt64FromStringMap(dataMap, "Id", 0)
+	if dataId <= 0 {
+		return nil
+	}
+	data := &models.LtUser{
+		Id:         int(dataId),
+		Username:   comm.GetStringFromStringMap(dataMap, "Username", ""),
+		Blacktime:  int(comm.GetInt64FromStringMap(dataMap, "Blacktime", 0)),
+		Realname:   comm.GetStringFromStringMap(dataMap, "Realname", ""),
+		Mobile:     comm.GetStringFromStringMap(dataMap, "Mobile", ""),
+		Address:    comm.GetStringFromStringMap(dataMap, "Address", ""),
+		SysCreated: int(comm.GetInt64FromStringMap(dataMap, "SysCreated", 0)),
+		SysUpdated: int(comm.GetInt64FromStringMap(dataMap, "SysUpdated", 0)),
+		SysIp:      comm.GetStringFromStringMap(dataMap, "SysIp", ""),
+	}
+	return data
+}
+
+// 将信息更新到缓存
+func (s *userService) setByCache(data *models.LtUser) {
+	if data == nil || data.Id <= 0 {
+		return
+	}
+	id := data.Id
+	key := fmt.Sprintf("info_user_%d", id)
+	rds := datasource.InstanceCache()
+	// 数据更新到redis缓存
+	params := redis.Args{key}
+	params = append(params, "Id", id)
+	if data.Username != "" {
+		params = append(params, "Username", data.Username)
+		params = append(params, "Blacktime", data.Blacktime)
+		params = append(params, "Realname", data.Realname)
+		params = append(params, "Mobile", data.Mobile)
+		params = append(params, "Address", data.Address)
+		params = append(params, "SysCreated", data.SysCreated)
+		params = append(params, "SysUpdated", data.SysUpdated)
+		params = append(params, "SysIp", data.SysIp)
+	}
+	_, err := rds.Do("HMSET", params...)
+	if err != nil {
+		log.Println("user_service.setByCache HMSET params=", params, ", error=", err)
+	}
+}
+
+// 数据更新了，直接清空缓存数据
+func (s *userService) updateByCache(data *models.LtUser, column []string) {
+	if data == nil || data.Id <= 0 {
+		return
+	}
+	// 集群模式，redis缓存
+	key := fmt.Sprintf("info_user_%d", data.Id)
+	rds := datasource.InstanceCache()
+	// 删除redis中的缓存
+	_, _ = rds.Do("DEL", key)
 }
